daeuniverse/outbound

commit c01ad3a54a38d6e63190774951e7d4fcbdb956a6

feat/fix: Update Hysteria2 client with new features and bug fixes (#21)

dialer/hysteria2/hysteria2.go
@@ -5,7 +5,7 @@ import (
 	"crypto/tls"
 	"crypto/x509"
 	"encoding/hex"
-	"errors"
+	"fmt"
 	"net"
 	"net/url"
 	"strconv"
@@ -14,6 +14,7 @@ import (
 	"github.com/daeuniverse/outbound/dialer"
 	"github.com/daeuniverse/outbound/netproxy"
 	"github.com/daeuniverse/outbound/protocol"
+	"github.com/daeuniverse/outbound/protocol/hysteria2/client"
 )
 
 func init() {
@@ -30,6 +31,8 @@ type Hysteria2 struct {
 	Insecure  bool
 	Sni       string
 	PinSHA256 string
+	MaxTx     uint64
+	MaxRx     uint64
 }
 
 func NewHysteria2(option *dialer.ExtraOption, nextDialer netproxy.Dialer, link string) (netproxy.Dialer, *dialer.Property, error) {
@@ -54,18 +57,34 @@ func (s *Hysteria2) Dialer(option *dialer.ExtraOption, nextDialer netproxy.Diale
 		Password: s.Password,
 		IsClient: true,
 	}
+	if header.SNI == "" {
+		header.SNI = s.Server
+	}
+	if s.MaxTx > 0 && s.MaxRx > 0 {
+		header.Feature1 = &client.BandwidthConfig{
+			MaxRx: s.MaxRx,
+			MaxTx: s.MaxTx,
+		}
+	} else if option.BandwidthMaxRx > 0 && option.BandwidthMaxTx > 0 {
+		header.Feature1 = &client.BandwidthConfig{
+			MaxRx: option.BandwidthMaxRx,
+			MaxTx: option.BandwidthMaxTx,
+		}
+	}
 	if s.PinSHA256 != "" {
 		nHash := normalizeCertHash(s.PinSHA256)
 		header.TlsConfig.VerifyPeerCertificate = func(rawCerts [][]byte, _ [][]*x509.Certificate) error {
+			certHashes := make([]string, 0, len(rawCerts))
 			for _, cert := range rawCerts {
 				hash := sha256.Sum256(cert)
 				hashHex := hex.EncodeToString(hash[:])
+				certHashes = append(certHashes, hashHex)
 				if hashHex == nHash {
 					return nil
 				}
 			}
 			// No match
-			return errors.New("no certificate matches the pinned hash")
+			return fmt.Errorf("no matching certificate found, %s not in %v", nHash, certHashes)
 		}
 	}
 	var err error
@@ -100,17 +119,34 @@ func ParseHysteria2URL(link string) (*Hysteria2, error) {
 	}
 	q := t.Query()
 	sni := q.Get("sni")
-	if sni == "" {
-		sni = t.Hostname()
+	var insecure bool
+	if insecureValue := q.Get("insecure"); insecureValue != "" {
+		insecure, err = strconv.ParseBool(q.Get("insecure"))
+		if err != nil {
+			return nil, dialer.InvalidParameterErr
+		}
+	}
+	var maxTx, maxRx uint64
+	if q.Get("maxTx") != "" && q.Get("maxRx") != "" {
+		maxTx, err = strconv.ParseUint(q.Get("maxTx"), 10, 64)
+		if err != nil {
+			return nil, dialer.InvalidParameterErr
+		}
+		maxRx, err = strconv.ParseUint(q.Get("maxRx"), 10, 64)
+		if err != nil {
+			return nil, dialer.InvalidParameterErr
+		}
 	}
 	conf := &Hysteria2{
 		Name:      t.Fragment,
 		User:      t.User.Username(),
 		Server:    t.Hostname(),
 		Port:      port,
-		Insecure:  q.Get("insecure") == "1",
+		Insecure:  insecure,
 		Sni:       sni,
 		PinSHA256: q.Get("pinSHA256"),
+		MaxTx:     maxTx,
+		MaxRx:     maxRx,
 	}
 	conf.Password, _ = t.User.Password()
 	return conf, nil
@@ -135,6 +171,10 @@ func (s *Hysteria2) ExportToURL() string {
 	}
 	if s.PinSHA256 != "" {
 		q.Set("pinSHA256", s.PinSHA256)
+	}
+	if s.MaxTx > 0 && s.MaxRx > 0 {
+		q.Set("maxTx", strconv.FormatUint(s.MaxTx, 10))
+		q.Set("maxRx", strconv.FormatUint(s.MaxRx, 10))
 	}
 	t.RawQuery = q.Encode()
 	return t.String()

dialer/dialer.go
@@ -11,6 +11,8 @@ type ExtraOption struct {
 	AllowInsecure     bool
 	TlsImplementation string
 	UtlsImitate       string
+	BandwidthMaxTx    uint64
+	BandwidthMaxRx    uint64
 }
 
 type Property struct {

protocol/header.go
@@ -5,7 +5,7 @@ import "crypto/tls"
 type Header struct {
 	ProxyAddress string
 	SNI          string
-	Feature1     string
+	Feature1     interface{}
 	TlsConfig    *tls.Config
 	Cipher       string
 	User         string

protocol/hysteria2/dialer.go
@@ -24,13 +24,7 @@ func NewDialer(nextDialer netproxy.Dialer, header protocol.Header) (netproxy.Dia
 		IsClient: header.IsClient,
 	}
 
-	serverAddr, err := net.ResolveUDPAddr("udp", header.ProxyAddress)
-	if err != nil {
-		return nil, err
-	}
-
 	config := &client.Config{
-		ServerAddr: serverAddr,
 		TLSConfig: client.TLSConfig{
 			ServerName:            header.TlsConfig.ServerName,
 			InsecureSkipVerify:    header.TlsConfig.InsecureSkipVerify,
@@ -42,10 +36,19 @@ func NewDialer(nextDialer netproxy.Dialer, header protocol.Header) (netproxy.Dia
 	if header.Password != "" {
 		config.Auth = header.User + ":" + header.Password
 	}
+	if feature := header.Feature1; feature != nil {
+		config.BandwidthConfig = *feature.(*client.BandwidthConfig)
+	}
 
 	client, err := client.NewReconnectableClient(
 		func() (*client.Config, error) {
-			return config, nil
+			serverAddr, err := net.ResolveUDPAddr("udp", header.ProxyAddress)
+			if err != nil {
+				return nil, err
+			}
+			newConfig := *config
+			newConfig.ServerAddr = serverAddr
+			return &newConfig, nil
 		},
 		func(c client.Client, hi *client.HandshakeInfo, i int) {
 			// Do nothing

protocol/juicity/dialer.go
@@ -63,7 +63,7 @@ func NewDialer(nextDialer netproxy.Dialer, header protocol.Header) (netproxy.Dia
 					},
 					Uuid:                 id,
 					Password:             header.Password,
-					CongestionController: header.Feature1,
+					CongestionController: header.Feature1.(string),
 					CWND:                 10,
 					Ctx:                  ctx,
 					Cancel:               cancel,

protocol/tuic/dialer.go
@@ -60,7 +60,7 @@ func NewDialer(nextDialer netproxy.Dialer, header protocol.Header) (netproxy.Dia
 					Uuid:                  id,
 					Password:              header.Password,
 					UdpRelayMode:          udpRelayMode,
-					CongestionController:  header.Feature1,
+					CongestionController:  header.Feature1.(string),
 					ReduceRtt:             false,
 					CWND:                  10,
 					MaxUdpRelayPacketSize: maxDatagramFrameSize,

protocol/vless/dialer.go
@@ -49,7 +49,7 @@ func NewDialer(nextDialer netproxy.Dialer, header protocol.Header) (netproxy.Dia
 		proxyAddress: header.ProxyAddress,
 		nextDialer:   nextDialer,
 		metadata:     metadata,
-		flow:         flow,
+		flow:         flow.(string),
 		// xudp:         header.Flags&protocol.Flags_VMess_UsePacketAddr == 0,
 		// xudp: true,
 		key: id,

protocol/vmess/dialer.go
@@ -47,7 +47,7 @@ func NewDialer(nextDialer netproxy.Dialer, header protocol.Header) (netproxy.Dia
 	return &Dialer{
 		proxyAddress:      header.ProxyAddress,
 		proxySNI:          header.SNI,
-		grpcServiceName:   header.Feature1,
+		grpcServiceName:   header.Feature1.(string),
 		nextDialer:        nextDialer,
 		metadata:          metadata,
 		key:               NewID(id).CmdKey(),

commit 79817c9a0ee215fd4b0efe33051594f5efac92f4

fix(hy2): dialer regression introduced by #21 pr
test uri: 'hy2://a@b.com:9527'

before this patch:

level=debug msg="Connectivity Check Failed" err="connect error: CRYPTO_ERROR 0x150 (remote): tls: internal error" network="tcp6(DNS)" node=kr02
level=debug msg="Connectivity Check Failed" err="connect error: CRYPTO_ERROR 0x150 (remote): tls: internal error" network="udp6(DNS)" node=kr02
level=debug msg="Connectivity Check Failed" err="connect error: CRYPTO_ERROR 0x150 (remote): tls: internal error" network="udp4(DNS)" node=kr02
level=debug msg="Connectivity Check Failed" err="connect error: CRYPTO_ERROR 0x150 (remote): tls: internal error" network="tcp4(DNS)" node=kr02

after this patch:

level=info msg="Group selects dialer" dialer=kr02 group=final network=tcp4
level=info msg="Group selects dialer" dialer=kr02 group=final network=tcp6
level=info msg="Group selects dialer" dialer=kr02 group=final network="udp4(DNS)"
level=info msg="Group selects dialer" dialer=kr02 group=final network="udp6(DNS)"
level=info msg="Group selects dialer" dialer=kr02 group=final network="tcp4(DNS)"
level=info msg="Group selects dialer" dialer=kr02 group=final network="tcp6(DNS)"

dialer/hysteria2/hysteria2.go
@@ -59,6 +59,7 @@ func (s *Hysteria2) Dialer(option *dialer.ExtraOption, nextDialer netproxy.Diale
 	}
 	if header.SNI == "" {
 		header.SNI = s.Server
+		header.TlsConfig.ServerName = s.Server
 	}
 	if s.MaxTx > 0 && s.MaxRx > 0 {
 		header.Feature1 = &client.BandwidthConfig{