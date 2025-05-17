0e506cd922f4449a47f38740a5e15f0d6007ebdd
cubatic45 <cubatic45@gmail.com>
2025-05-18 01:46:09 +0800

    add anytls tcp without padding

---
+++ protocol/anytls/anytls.go
@@ -0,0 +1,168 @@
+package anytls
+
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"io"
+	"strconv"
+	"sync"
+
+	"github.com/daeuniverse/outbound/netproxy"
+	"github.com/daeuniverse/outbound/pool"
+	"github.com/daeuniverse/outbound/protocol"
+	"github.com/daeuniverse/outbound/protocol/infra/socks"
+)
+
+const ( // cmds
+	cmdWaste               = iota // Paddings
+	cmdSYN                        // stream open
+	cmdPSH                        // data push
+	cmdFIN                        // stream close, a.k.a EOF mark
+	cmdSettings                   // Settings (Client send to Server)
+	cmdAlert                      // Alert
+	cmdUpdatePaddingScheme        // update padding scheme
+	// Since version 2
+	cmdSYNACK         // Server reports to the client that the stream has been opened
+	cmdHeartRequest   // Keep alive command
+	cmdHeartResponse  // Keep alive command
+	cmdServerSettings // Settings (Server send to client)
+)
+
+const (
+	headerOverHeadSize = 1 + 4 + 2
+)
+
+// frame defines a packet from or to be multiplexed into a single connection
+type frame struct {
+	cmd  byte   // 1
+	sid  uint32 // 4
+	data []byte // 2 + len(data)
+}
+
+func newFrame(cmd byte, sid uint32) frame {
+	return frame{cmd: cmd, sid: sid}
+}
+
+type rawHeader [headerOverHeadSize]byte
+
+func (h rawHeader) Cmd() byte {
+	return h[0]
+}
+
+func (h rawHeader) StreamID() uint32 {
+	return binary.BigEndian.Uint32(h[1:])
+}
+
+func (h rawHeader) Length() uint16 {
+	return binary.BigEndian.Uint16(h[5:])
+}
+
+func writeFrame(conn io.Writer, frame frame) (int, error) {
+	dataLen := len(frame.data)
+
+	buffer := pool.Get(dataLen + headerOverHeadSize)
+	defer pool.Put(buffer)
+
+	buffer[0] = frame.cmd
+	binary.BigEndian.PutUint32(buffer[1:], frame.sid)
+	binary.BigEndian.PutUint16(buffer[5:], uint16(dataLen))
+	copy(buffer[7:], frame.data)
+	_, err := conn.Write(buffer)
+	if err != nil {
+		return 0, err
+	}
+
+	return dataLen, nil
+}
+
+var _ netproxy.Conn = (*anytlsConn)(nil)
+
+type anytlsConn struct {
+	netproxy.Conn
+	buf        bytes.Buffer
+	metadata   protocol.Metadata
+	writeMutex sync.Mutex
+	readMutex  sync.Mutex
+
+	sid uint32
+}
+
+func (d *Dialer) newAnytlsConn(conn netproxy.Conn, metadata protocol.Metadata) (netproxy.Conn, error) {
+	password := d.key
+	b := make([]byte, len(password)+2)
+	copy(b, password)
+	binary.BigEndian.PutUint16(b[len(password):], uint16(0))
+	if _, err := conn.Write(b); err != nil {
+		return nil, err
+	}
+	frame := newFrame(cmdSettings, d.sid.Load())
+	frame.data = settingsBytes
+	if _, err := writeFrame(conn, frame); err != nil {
+		return nil, err
+	}
+	d.sid.Add(1)
+
+	frame = newFrame(cmdSYN, d.sid.Load())
+	if _, err := writeFrame(conn, frame); err != nil {
+		return nil, err
+	}
+
+	tgtAddr, err := socks.ParseAddr(metadata.Hostname + ":" + strconv.Itoa(int(metadata.Port)))
+	if err != nil {
+		return nil, err
+	}
+	frame = newFrame(cmdPSH, d.sid.Load())
+	frame.data = tgtAddr
+	if _, err := writeFrame(conn, frame); err != nil {
+		return nil, err
+	}
+
+	return &anytlsConn{Conn: conn, buf: bytes.Buffer{}, metadata: metadata, sid: d.sid.Load()}, nil
+}
+
+func (c *anytlsConn) Write(b []byte) (n int, err error) {
+	c.writeMutex.Lock()
+	defer c.writeMutex.Unlock()
+
+	frame := newFrame(cmdPSH, c.sid)
+	frame.data = b
+	return writeFrame(c.Conn, frame)
+}
+
+func (c *anytlsConn) Read(b []byte) (n int, err error) {
+	c.readMutex.Lock()
+	defer c.readMutex.Unlock()
+
+	return c.Conn.Read(b)
+
+	var header rawHeader
+	if _, err := io.ReadFull(c.Conn, header[:]); err != nil {
+		return 0, err
+	}
+	var buf []byte
+	if header.Cmd() <= cmdServerSettings || header.Cmd() >= cmdWaste {
+		buf = pool.Get(int(header.Length()))
+		defer pool.Put(buf)
+	}
+	switch header.Cmd() {
+	case cmdWaste:
+		io.ReadFull(c.Conn, buf)
+		return 0, nil
+	case cmdSYN:
+		if c.buf.Len() > 0 {
+			return c.buf.Read(b)
+		}
+		if _, err := io.ReadFull(c.Conn, buf); err != nil {
+			return 0, err
+		}
+		c.buf.Write(buf)
+		return c.buf.Read(b)
+	case cmdUpdatePaddingScheme:
+	case cmdAlert:
+		return 0, fmt.Errorf("alert: %s", string(buf))
+	default:
+		return 0, fmt.Errorf("invalid cmd: %d", header.Cmd())
+	}
+	return 0, nil
+}
---
+++ protocol/anytls/dialer.go
@@ -0,0 +1,80 @@
+package anytls
+
+import (
+	"context"
+	"crypto/sha256"
+	"crypto/tls"
+	"fmt"
+	"net"
+	"sync/atomic"
+
+	"github.com/daeuniverse/outbound/netproxy"
+	"github.com/daeuniverse/outbound/protocol"
+)
+
+func init() {
+	protocol.Register("anytls", NewDialer)
+}
+
+type Dialer struct {
+	proxyAddress string
+	nextDialer   netproxy.Dialer
+	metadata     protocol.Metadata
+	key          []byte
+	tlsConfig    *tls.Config
+	sid          atomic.Uint32
+}
+
+func NewDialer(nextDialer netproxy.Dialer, header protocol.Header) (netproxy.Dialer, error) {
+	metadata := protocol.Metadata{
+		IsClient: header.IsClient,
+	}
+	sum := sha256.Sum256([]byte(header.Password))
+	return &Dialer{
+		proxyAddress: header.ProxyAddress,
+		nextDialer:   nextDialer,
+		metadata:     metadata,
+		key:          sum[:],
+		tlsConfig:    header.TlsConfig,
+	}, nil
+}
+
+func (d *Dialer) DialTcp(ctx context.Context, addr string) (c netproxy.Conn, err error) {
+	return d.DialContext(ctx, "tcp", addr)
+}
+
+func (d *Dialer) DialUdp(ctx context.Context, addr string) (c netproxy.PacketConn, err error) {
+	pktConn, err := d.DialContext(ctx, "udp", addr)
+	if err != nil {
+		return nil, err
+	}
+	return pktConn.(netproxy.PacketConn), nil
+}
+
+func (d *Dialer) DialContext(ctx context.Context, network string, addr string) (c netproxy.Conn, err error) {
+	magicNetwork, err := netproxy.ParseMagicNetwork(network)
+	if err != nil {
+		return nil, err
+	}
+	switch magicNetwork.Network {
+	case "tcp", "udp":
+		mdata, err := protocol.ParseMetadata(addr)
+		if err != nil {
+			return nil, err
+		}
+		mdata.IsClient = d.metadata.IsClient
+
+		tcpNetwork := netproxy.MagicNetwork{
+			Network: "tcp",
+			Mark:    magicNetwork.Mark,
+			Mptcp:   magicNetwork.Mptcp,
+		}.Encode()
+		conn, err := d.nextDialer.DialContext(ctx, tcpNetwork, d.proxyAddress)
+		if err != nil {
+			return nil, err
+		}
+		return d.newAnytlsConn(tls.Client(conn.(net.Conn), d.tlsConfig), mdata)
+	default:
+		return nil, fmt.Errorf("%w: %v", netproxy.UnsupportedTunnelTypeError, magicNetwork.Network)
+	}
+}
