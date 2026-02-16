e43a84ca1343a49390fe1da3e2ed64dce4b3d078
FN <firenamefn@gmail.com>
2026-02-16 18:13:30 +0700

    style: remove obsolete todo comment

--- dialer/hysteria2/hysteria2.go
+++ dialer/hysteria2/hysteria2.go
@@ -128,7 +128,6 @@ func normalizeCertHash(hash string) string {
 
 // ref: https://v2.hysteria.network/zh/docs/developers/URI-Scheme/
 func ParseHysteria2URL(link string) (*Hysteria2, error) {
-	// TODO: support salamander obfuscation
 	u, err := url.Parse(link)
 	if err != nil {
 		return nil, err
